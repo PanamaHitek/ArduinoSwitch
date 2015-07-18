c962a366a9bf4eb14d414bd5d41610cd359125e6
Antony <Antony@SonyVaio>
2015-07-18 11:29:46 -0500

    Proyecto adaptado a Maven

---
+++ Java/ArduinoSwitch/src/main/java/com/panamahitek/arduinoswitch/ArduinoSwitch.java
@@ -0,0 +1,13 @@
+package com.panamahitek.arduinoswitch;
+
+public class ArduinoSwitch {
+
+    //Se crea una instancia del JFrame para que la interfaz gráfica
+    public static void main(String[] args) {
+        Window Ventana = new Window();
+
+        Ventana.setVisible(true);
+
+    }
+
+}
